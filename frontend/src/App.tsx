8fe79b62c9d46cad16cacb14b51c9662eb899042
Joshua Charles <josh@asleepingturtle.com>
2024-12-07 22:28:52 -0500

    vite built frontend folder react and mui

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@mui/material'
+
+function App() {
+
+  return (
+    <div>
+      <h1>ClearOps</h1>
+      <Button variant='contained'>Test Button</Button>
+    </div>
+  )
+}
+
+export default App
